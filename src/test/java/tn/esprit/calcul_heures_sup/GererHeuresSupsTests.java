c38d2385260c9610bc6403f308709709f0f05fa1
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2025-03-13 17:04:32 +0100

    Test class on business

---
+++ src/test/java/tn/esprit/calcul_heures_sup/GererHeuresSupsTests.java
@@ -0,0 +1,12 @@
+package tn.esprit.calcul_heures_sup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GererHeuresSupsTests {
+    @Test
+    public void getAllHeuresSup_badRequest_returnNull(){
+
+    }
+}
