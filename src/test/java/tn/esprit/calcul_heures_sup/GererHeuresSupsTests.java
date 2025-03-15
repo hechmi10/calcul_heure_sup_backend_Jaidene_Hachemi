dbc6776dc4140634d47e03a6fe38d298cdaaf34f
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2025-03-15 13:22:42 +0100

    Adding other test methods to the test class

--- src/test/java/tn/esprit/calcul_heures_sup/GererHeuresSupsTests.java
+++ src/test/java/tn/esprit/calcul_heures_sup/GererHeuresSupsTests.java
@@ -2,11 +2,47 @@ package tn.esprit.calcul_heures_sup;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.calcul_heures_sup.entities.HeureSup;
+import tn.esprit.calcul_heures_sup.services.HeureSupServiceImpl;
+import tn.esprit.calcul_heures_sup.services.IHeureSupService;
+
+import java.util.List;
+import java.util.Set;
 
 @SpringBootTest
 class GererHeuresSupsTests {
     @Test
-    public void getAllHeuresSup_badRequest_returnNull(){
+    public void getAllHeuresSup_badRequest_returnFalse(){
+        IHeureSupService heureSupService = new HeureSupServiceImpl();
+        Set<HeureSup> hs=heureSupService.getHeureSups();
+        assert null != hs;
+    }
 
+    @Test
+    public void addHeuresSup_badRequest_returnFalse(){
+        IHeureSupService heureSupService = new HeureSupServiceImpl();
+        HeureSup hs=heureSupService.addHeureSup(new HeureSup());
+        assert null != hs;
+    }
+
+    @Test
+    public void updateHeuresSup_badRequest_returnFalse(){
+        IHeureSupService heureSupService = new HeureSupServiceImpl();
+        HeureSup hs=heureSupService.updateHeureSup(1,new HeureSup());
+        assert null != hs;
+    }
+
+    @Test
+    public void deleteHeuresSup_badRequest_returnFalse(){
+        IHeureSupService heureSupService = new HeureSupServiceImpl();
+        heureSupService.deleteHeureSup(1);
+        assert false;
+    }
+
+    @Test
+    public void getHeuresSup_badRequest_returnFalse(){
+        IHeureSupService heureSupService = new HeureSupServiceImpl();
+        HeureSup hs=heureSupService.getHeureSup(1);
+        assert null != hs;
     }
 }
